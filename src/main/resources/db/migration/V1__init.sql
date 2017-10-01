52a2376c5d92b17c643d8445633827650178c4f2
apgv <ap@foorbar.codes>
2017-10-01 22:52:14 +0200

    Added SQL test data

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,8 @@
+CREATE TABLE employer (
+  id      SERIAL CONSTRAINT employer_pk PRIMARY KEY,
+  name    VARCHAR(40) NOT NULL,
+  started DATE        NOT NULL,
+  ended   DATE        NOT NULL
+);
+
+INSERT INTO employer (name, started, ended) VALUES ('Vedal-IT', '2003-01-01', '2005-12-31')
