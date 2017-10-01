51e9a6387b119fdc464b1bd7c9ed1f6b67001b39
apgv <ap@foorbar.codes>
2017-10-01 22:57:04 +0200

    Print database content to console

--- src/main/kotlin/codes/foobar/cv/Main.kt
+++ src/main/kotlin/codes/foobar/cv/Main.kt
@@ -1,12 +1,36 @@
 package codes.foobar.cv
 
 import com.google.gson.Gson
-import spark.Spark.*
+import com.zaxxer.hikari.HikariDataSource
+import org.jooq.SQLDialect
+import org.jooq.codes.foobar.cv.Tables.EMPLOYER
+import org.jooq.impl.DSL
+import spark.Spark.get
+import javax.sql.DataSource
 
 fun main(args: Array<String>) {
 
     get("/hello", { _, response ->
+
+        val create = DSL.using(datasource(), SQLDialect.POSTGRES)
+        create.select().from(EMPLOYER).fetch().forEach {
+            println("ID=${it[EMPLOYER.ID]}, " +
+                    "NAME=${it[EMPLOYER.NAME]}, " +
+                    "STARTED=${it[EMPLOYER.STARTED]}, " +
+                    "ENDED=${it[EMPLOYER.ENDED]}")
+        }
+
         response.type("application/json")
         Gson().toJson(mapOf("greeting" to "hello world"))
     })
+}
+
+fun datasource(): DataSource {
+    return HikariDataSource().apply {
+        jdbcUrl = "jdbc:h2:./cv_db"
+        driverClassName = "org.h2.Driver"
+        schema = "public"
+        username = "sa"
+        password = ""
+    }
 }
