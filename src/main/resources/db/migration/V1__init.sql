ebb005e3d5bb3779e1c34df63ea36fb4933a8a83
apgv <ap@foorbar.codes>
2017-10-11 21:40:03 +0200

    Added more tables to represent to different sections of the CV

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,8 +1,49 @@
-CREATE TABLE employer (
-  id      SERIAL CONSTRAINT employer_pk PRIMARY KEY,
-  name    VARCHAR(40) NOT NULL,
-  started DATE        NOT NULL,
-  ended   DATE        NOT NULL
+CREATE TABLE person (
+  id   SERIAL CONSTRAINT person_pk PRIMARY KEY,
+  name VARCHAR(60) NOT NULL
 );
 
-INSERT INTO employer (name, started, ended) VALUES ('Vedal-IT', '2003-01-01', '2005-12-31');
+CREATE TABLE experience (
+  id          SERIAL CONSTRAINT experience_pk PRIMARY KEY,
+  person_id   INTEGER REFERENCES person (id),
+  start_date  DATE NOT NULL,
+  end_date    DATE,
+  description VARCHAR(60)
+);
+
+CREATE TABLE expertise (
+  id        SERIAL CONSTRAINT expertise_pk PRIMARY KEY,
+  person_id INTEGER REFERENCES person (id),
+  label     VARCHAR(25)  NOT NULL,
+  content   VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE employer (
+  id         SERIAL CONSTRAINT employer_pk PRIMARY KEY,
+  person_id  INTEGER REFERENCES person (id),
+  name       VARCHAR(40) NOT NULL,
+  start_date DATE        NOT NULL,
+  end_date   DATE        NOT NULL
+);
+
+CREATE TABLE project (
+  id          SERIAL CONSTRAINT project_pk PRIMARY KEY,
+  name        VARCHAR(40)  NOT NULL,
+  employer_id INTEGER REFERENCES employer (id),
+  start_date  DATE         NOT NULL,
+  end_date    DATE,
+  role        VARCHAR(40)  NOT NULL,
+  description VARCHAR(255) NOT NULL,
+  tech        VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE education (
+  id          SERIAL CONSTRAINT education_pk PRIMARY KEY,
+  person_id   INTEGER REFERENCES person (id),
+  course      BOOLEAN      NOT NULL,
+  start_date  DATE         NOT NULL,
+  end_date    DATE,
+  description VARCHAR(500) NOT NULL
+);
+
+INSERT INTO employer (name, start_date, end_date) VALUES ('Vedal-IT', '2003-01-01', '2005-12-31');
